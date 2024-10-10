ab5c1c16b2ef040aec3330ce49ddd8c4d8cdb08b
pedrocruz <JayJMarquez05@gmail.com>
2024-10-10 13:43:30 -0500

    Reviews modules is now built. Might ask a question later about something though

--- models/reviews.js
+++ models/reviews.js
@@ -1,1 +1,12 @@
 const Schema = require(`mongoose`);
+
+const ReviewSchema = new Schema(
+  {
+    username: { type: String, required: true },
+    score: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+module.exports = ReviewSchema;
